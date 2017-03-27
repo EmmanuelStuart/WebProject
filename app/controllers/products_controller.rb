242ef8e75bb0a5b12f27ca4cad7203165f565b56
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-03-27 05:03:21 +0000

    removes the search, not gonna do that now, too hard

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,23 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    # @tasks = if params[:search]
+    #   Product.where('name LIKE %?%', "%#{params[:term]}%")
+    # else
+    #   Product.all
+    # end
+    # @products = Product.search(params[:term])
   end
 
   def display
     @product = Product.find(params[:number].to_i)
   end
+
+
+
+  # def task_params
+  #   params.require(:task).permit(:name, :term)
+  # end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, :price, presence: true
+
+  # def self.search(search)
+  #   if search
+  #     where('name LIKE ?', "%#{term}%").order('id DESC')
+  #   else
+  #     all
+  #   end
+  # end
+
+
 end
