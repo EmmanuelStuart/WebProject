62a1d6e0b70a9fd03eeca28d85ba5b3f662c64e4
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-03-29 18:27:15 +0000

    added ability to search by productName and productInfo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,30 @@ class ProductsController < ApplicationController
     @category = Category.all
 
     if params[:search]
-      @products = Product.where('productName LIKE ?', "%#{params[:search]}%")
+      # @products = Product.where('productName LIKE ?', "%#{params[:search]}%")
+      # @products = Product.where('category LIKE ? ', "#{params[:search]}")
+      # @products = Product.where("category = 'cpu'")
+
+      # @category.each do |c|
+      #   @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'
+      #   OR category_id LIKE '%#{params[:search]}%'")
+      # end
+
+
+      # @categorySearch = Category.find('id').where("name LIKE '%#{params[:search]}%'")
+
+
+      # working
+      @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'")
+
+      # @categorySearch = Category.find_by("name LIKE '%#{params[:search]}%'")
+      # @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'
+      # OR category_id IS '%#{@categorySearch}%'")
+
+
+
+
+
     elsif params[:category_id]
       @products = Product.where("category_id = #{ params[:category_id] }")
     else
