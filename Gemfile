7f008bf0a74f97a45856d9ef697732c7c482d495
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-03-30 18:24:28 +0000

    added pagination

--- Gemfile
+++ Gemfile
@@ -59,3 +59,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
     gem 'devise'
 
     gem 'carrierwave', '~> 1.0'
+
+
+#pagination
+gem 'kaminari'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,14 +7,14 @@ class ProductsController < ApplicationController
 
     if params[:search]
       if params[:category_id] == "all"
-        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'").order(:productName)
+        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'").order(:productName).page(params[:page]).per(1)
       else
         # @products = Product.where("category_id LIKE '%#{params[:category_id]}%'").search(params[:search])
-        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").order(:productName)
+        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").order(:productName).page(params[:page]).per(1)
         # @products = Product.all
       end
     else
-      @products = Product.all
+      @products = Product.all.page(params[:page]).per(2)
     end
   end
 
