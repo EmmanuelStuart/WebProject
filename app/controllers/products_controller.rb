7e03e4618ddea35318fb3d40e552ec98effddde6
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-03-30 18:16:27 +0000

    added another product, ADDED SORTING RESULTS

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,10 +7,10 @@ class ProductsController < ApplicationController
 
     if params[:search]
       if params[:category_id] == "all"
-        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'")
+        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'").order(:productName)
       else
         # @products = Product.where("category_id LIKE '%#{params[:category_id]}%'").search(params[:search])
-        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'")
+        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").order(:productName)
         # @products = Product.all
       end
     else
