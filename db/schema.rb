b1cc7065550313fd238ceb5ff7672e148f3a6217
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-04-05 17:20:35 +0000

    populated the province, added another column on province

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405153522) do
+ActiveRecord::Schema.define(version: 20170405165835) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170405153522) do
     t.decimal  "gst"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "hst"
   end
 
   create_table "users", force: :cascade do |t|
