77f46c3383fe72c75310a1b7bcb0cc11e95cc902
Emmanuel Moncada <emmanuelstuartmoncada@gmail.com>
2017-04-06 17:23:55 +0000

    added another category called MOTHERBOAR, and added products to it

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,14 +7,14 @@ class ProductsController < ApplicationController
 
     if params[:search]
       if params[:category_id] == "all"
-        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'").order(:productName).page(params[:page]).per(3)
+        @products = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%'").order(:productName).page(params[:page]).per(6)
       else
         # @products = Product.where("category_id LIKE '%#{params[:category_id]}%'").search(params[:search])
-        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").order(:productName).page(params[:page]).per(3)
+        @products = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").order(:productName).page(params[:page]).per(6)
         # @products = Product.all
       end
     elsif params[:category_type]
-      @products = Product.where("category_id = #{params[:category_type]}").order(:productName).page(params[:page]).per(3)
+      @products = Product.where("category_id = #{params[:category_type]}").order(:productName).page(params[:page]).per(6)
     else
       @products = Product.all.page(params[:page]).per(6)
     end
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
   end
 
   def recentlyUpdated
-    @productsRecentlyUpdated = Product.where('updated_at >= :three_days_ago', :three_days_ago => Time.now - 3.days).order(updated_at: :desc).page(params[:page]).per(3)
+    @productsRecentlyUpdated = Product.where('updated_at >= :three_days_ago', :three_days_ago => Time.now - 3.days).order(updated_at: :desc).page(params[:page]).per(6)
 
     # @productsRecentlyUpdated = Product.where('created_at >= :seven_days_ago or updated_at >= :three_days_ago',
     #       :seven_days_ago  => Time.now - 7.days,
@@ -34,29 +34,29 @@ class ProductsController < ApplicationController
 
     if params[:search]
       if params[:category_id] == "all"
-        @productsRecentlyUpdated = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%' AND created_at = #{Time.now - 30.days} AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(3)
+        @productsRecentlyUpdated = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%' AND created_at = #{Time.now - 30.days} AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(6)
       else
         # @products = Product.where("category_id LIKE '%#{params[:category_id]}%'").search(params[:search])
-        @productsRecentlyUpdated = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%' AND created_at = #{Time.now - 30.days } AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(3)
+        @productsRecentlyUpdated = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%' AND created_at = #{Time.now - 30.days } AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(6)
       end
     else
-      @products = Product.all.page(params[:page]).per(2)
+      @products = Product.all.page(params[:page]).per(6)
     end
   end
 
   def new
     # @new = Product.where("created_at >= #{Time.now - 15.days}").order(:productName).page(params[:page]).per(3)
-    @new = Product.where('created_at >= :two_days_ago', :two_days_ago => Time.now - 2.days).order(:created_at).page(params[:page]).per(3)
+    @new = Product.where('created_at >= :two_days_ago', :two_days_ago => Time.now - 2.days).order(created_at: :desc).page(params[:page]).per(6)
 
     if params[:search]
       if params[:category_id] == "all"
-        @new = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%' AND created_at = #{Time.now - 30.days} AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(3)
+        @new = Product.where("productName LIKE '%#{params[:search]}%' OR productInfo LIKE '%#{params[:search]}%' AND created_at = #{Time.now - 30.days} AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(6)
       else
         # @products = Product.where("category_id LIKE '%#{params[:category_id]}%'").search(params[:search])
-        @new = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%' AND created_at = #{Time.now - 30.days } AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(3)
+        @new = Product.where("productName LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%' AND created_at = #{Time.now - 30.days } AND updated_at = #{Time.now - 7.days}").order(:productName).page(params[:page]).per(6)
       end
     else
-      @products = Product.all.page(params[:page]).per(2)
+      @products = Product.all.page(params[:page]).per(6)
     end
   end
 
